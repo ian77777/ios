60c89664429314da5b13fd066e8d84cdb0bc9044
定风波 <bingye.yu@alipay.com>
2014-03-20 11:30:40 +0800

    init funny camera

---
+++ Funny Camera/Funny Camera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Funny Camera
+//
+//  Created by 凯子 on 14-3-20.
+//  Copyright (c) 2014年 Bingye Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YKFCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YKFCAppDelegate class]));
+    }
+}
---
+++ Funny Camera/Funny CameraTests/Funny_CameraTests.m
@@ -0,0 +1,34 @@
+//
+//  Funny_CameraTests.m
+//  Funny CameraTests
+//
+//  Created by 凯子 on 14-3-20.
+//  Copyright (c) 2014年 Bingye Yu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Funny_CameraTests : XCTestCase
+
+@end
+
+@implementation Funny_CameraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
