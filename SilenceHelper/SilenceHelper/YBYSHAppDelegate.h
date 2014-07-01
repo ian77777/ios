97afb87955d7f3d777765d5469034405f39e7439
定风波 <bingye.yu@alipay.com>
2014-07-01 14:39:43 +0800

    add SilenceHelper

---
+++ SilenceHelper/SilenceHelper/YBYSHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YBYSHAppDelegate.h
+//  SilenceHelper
+//
+//  Created by yukai44444 on 14-6-26.
+//  Copyright (c) 2014年 Bingye Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBYSHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SilenceHelper/SilenceHelper/YBYSHRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  YBYSHRootViewController.h
+//  SilenceHelper
+//
+//  Created by yukai44444 on 14-6-26.
+//  Copyright (c) 2014年 Bingye Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBYSHRootViewController : UIViewController
+
+@end
